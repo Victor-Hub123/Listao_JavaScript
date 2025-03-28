0e9d05a61e7e27ba906160dc5bfe9e107efdb1de
Victor Oliveira <victorcriri313@gmail.com>
2025-03-28 11:55:57 -0300

    feat(operadores-logicos): adiciona verificação de números positivos

---
+++ Operadores_Logicos/AmbosPositivos.js
@@ -0,0 +1,11 @@
+let Num1 = -50;
+let Num2 = 1;
+
+switch (true){
+    case Num1 && Num2 > 0:
+        console.log(`Sim`);
+        break
+    case Num1 && Num2 < 0:
+        console.log(`Não`);
+        break
+};
