8deb11908b937ff3286479eca76f937e02bdd3e3
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-04-03 14:15:14 -0300

    feat(if-else): adiciona comparação do maior entre dois números

---
+++ If_Else/OMaiorVence.js
@@ -0,0 +1,10 @@
+let Num1 = 10;
+let Num2 = 20;
+
+if (Num1 > Num2){
+    console.log(`${Num1} é maior que ${Num2}`);
+}
+else if (Num2 > Num1){
+    console.log(`${Num2} é maior que ${Num1}`);
+}
+else (console.log('Os números são iguais'));
