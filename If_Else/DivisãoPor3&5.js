140fc61d69201bd447c98c544c77ff2ddb716f26
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-04-03 14:30:08 -0300

    feat(if-else): adiciona verificação de divisibilidade por 3 e 5

---
+++ If_Else/DivisãoPor3&5.js
@@ -0,0 +1,9 @@
+let Num = 5;
+
+if (Num % 3 == 0){
+    console.log('O número é divisível por 3');
+}
+else if (Num % 5 == 0){
+    console.log('O número é divisível por 5');
+}
+else (console.log('Não é divisível nem por 3 e nem por 5'));
