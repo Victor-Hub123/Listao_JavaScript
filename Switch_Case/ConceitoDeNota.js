79c665166015503d7047533ed887d27b009e5137
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-04-03 17:18:30 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/ConceitoDeNota.js
@@ -0,0 +1,19 @@
+let nota = 3;
+
+switch (true){
+    case nota <= 2:
+        console.log('Nota E');
+        break;
+    case nota <= 4:
+        console.log('Nota D');
+        break;
+    case nota <= 6:
+        console.log('Nota C');
+        break;
+    case nota <= 8:
+        console.log('Nota B');
+        break;
+    case nota <= 10:
+        console.log('Nota A');
+        break;
+}
