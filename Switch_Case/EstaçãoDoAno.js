ac256834755aeccfaf31ba7e2b7977654fb093b4
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-04-03 17:23:31 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/EstaçãoDoAno.js
@@ -0,0 +1,16 @@
+let Estação = 2;
+
+switch (Estação){
+    case 1:
+        console.log('Verão');
+        break;
+    case 2:
+        console.log('Outono');
+        break;
+    case 3:
+        console.log('Inverno');
+        break;
+    case 4:
+        console.log('Primavera');
+        break;
+}
