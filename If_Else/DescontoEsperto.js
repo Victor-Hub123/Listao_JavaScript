8aebc0bc9764dce4500ff8c63044ec206c8c1387
Victor Ferreira <victorfoliveira0407@gmail.com>
2025-04-03 15:03:53 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

---
+++ If_Else/DescontoEsperto.js
@@ -0,0 +1,8 @@
+let Salário = 2500;
+
+if (Salário > 2000){
+    console.log(`O salário final com 10% de aumento é ${Salário * 1.10}`);
+}
+else if (Salário <= 2000){
+    console.log(`O salário final com 5% de aumento é ${Salário * 1.5}`);
+}
